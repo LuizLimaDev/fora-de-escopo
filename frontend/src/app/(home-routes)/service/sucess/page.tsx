3b8c4de445297c94138d1510518041be1500b1d3
LuizLimaDev <devluizlime@gmail.com>
2024-10-16 11:13:33 -0300

    feat: sucess page

---
+++ frontend/src/app/(home-routes)/service/sucess/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ModalSucess = () => {
+  return (
+    <main className="w-screen h-screen flex-col-center">
+      <div className="w-[90%] rounded-3xl px-6 py-20 flex-col-center gap-20 bg-linx-white">
+        <h1 className="font-dosis font-bold text-linx-purple text-5xl text-center">
+          Chamado registrado
+        </h1>
+        <p className="font-bold text-linx-orange text-5xl">5265</p>
+        <p className="font-bold text-linx-dark-gray text-2xl text-center">
+          Entre em contato com o Suporte Degust e informe o número do chamado
+          Fora de Escopo.
+        </p>
+
+        <Link href="https://wa.me/1121034321">
+          <Image
+            src="/icons/whats.svg"
+            alt="contato do suport por whatsapp"
+            width={107}
+            height={100}
+          />
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default ModalSucess;
