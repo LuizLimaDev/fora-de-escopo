3ed070a9d5294fc4545026f48359ad5a03fe9c36
LuizLimaDev <devluizlime@gmail.com>
2024-11-09 23:20:18 -0300

    fix: api url from api awake

--- backend/src/services/apiAwake.js
+++ backend/src/services/apiAwake.js
@@ -1,21 +1,13 @@
 import https from "https";
 
 export default function apiAwake() {
-  const url = "https://example.com/api/data"; // Replace with your API endpoint
+  const url = "https://fora-de-escopo-api.onrender.com/tp";
 
   https
     .get(url, (response) => {
-      let data = "";
-
-      // A chunk of data has been received.
-      response.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      // The response has ended, so log the data.
       response.on("end", () => {
         try {
-          console.log("Data fetched:", JSON.parse(data)); // If the response is JSON
+          console.log("Awaked!"); // If the response is JSON
         } catch (error) {
           console.error("Error parsing JSON:", error.message);
         }
