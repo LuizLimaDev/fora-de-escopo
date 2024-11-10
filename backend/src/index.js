ae45d674c7a96a91256b33a14f50e4d0c74c541d
LuizLimaDev <devluizlime@gmail.com>
2024-11-09 23:27:13 -0300

    fix: apiAwake path

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import routes from "./routes/routes.js";
-import apiAwake from "./services/apiAwake";
+import apiAwake from "./services/apiAwake.js";
 import dbConnection from "./services/dbConnection.js";
 
 setInterval(apiAwake, 297000);
